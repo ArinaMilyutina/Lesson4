1220a806a8dcf059dc41f9d9e0d6e419fec986ba
Arina <arina.milyutina.0005@gmail.com>
2022-12-01 00:48:15 +0300

    commit1

---
+++ src/Work7.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Work7 {
+    public static void main(String[]args){
+        String[]array={"Arina","Polina","Sasha","Julia","Liza"};
+        System.out.println("This array:   "+Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Sorted array: "+Arrays.toString(array));
+    }
+}
