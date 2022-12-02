e589c99793466e191e16a7f34d8047be6ad55da2
Arina <arina.milyutina.0005@gmail.com>
2022-12-02 22:51:02 +0300

    commit1

--- src/Work2.java
+++ src/Work2.java
@@ -2,22 +2,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Work2 {
     public static void main(String[] args) {
-        int[] array1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 22};
-        int[] array2 = null;
+        int[] array1 = new int[]{2, 2, 4, 5, 6, 7, 8, 9, 22};
+        int count = 0;
         System.out.print("Enter the number,please.");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        for (int i = 0; i < array1.length; i++) {
-                array2 = new int[array1.length - 1];
-                System.arraycopy(array1, 0, array2, 0, i);
-                System.arraycopy(array1, i + 1, array2, i, array1.length - 1 - i);
+        for (int k : array1) {
+            if (k != number) {
+                count++;
             }
-        if (array1[9] == number) {
-            System.out.println("New array-" + Arrays.toString(array2));
-        } else {
-            System.out.println("The entered number is not in the array.");
         }
+        int[] array2 = new int[count];
+        int index = 0;
+        for (int j : array1)
+            if (j != number) {
+                array2[index] = j;
+                index++;
+
+            }
+        System.out.println(Arrays.toString(array2));
+
+
     }
 }
-
 
